98b4802b6fac16aaea493aef816f9f064bb5528e
pranali0831 <pranalibhanage081@gmail.com>
2024-07-09 19:20:02 +0530

    Update Employee.java
    m1 method added

--- Employee_Java_Project/src/emp/Employee.java
+++ Employee_Java_Project/src/emp/Employee.java
@@ -1,6 +1,11 @@
 package emp;
 
 public class Employee {
+
+	public void m1()
+	{
+		System.out.println("Employee class");
+	}
 	public static void main(String[] args) {
 		System.out.println("this is employee class");
 	}
