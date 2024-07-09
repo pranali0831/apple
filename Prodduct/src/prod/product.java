14d28667e1f2eb5d8dea12d0a7d1ace3e3653716
pranali0831 <pranalibhanage081@gmail.com>
2024-07-09 19:02:39 +0530

    Update product.java
    method m1 added

--- Prodduct/src/prod/product.java
+++ Prodduct/src/prod/product.java
@@ -1,6 +1,11 @@
 package prod;
 
 public class product {
+
+	public void m1()
+	{
+		system.out.println("m1-method");
+	}
 	public static void main(String[] args) {
 		System.out.println("mainproduct");
 	}
